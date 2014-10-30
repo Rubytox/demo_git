2bf20fd75ca34f74c3fc11d3503c22ab23a08943
Christophe Néraud <c.fag@hotmail.fr>
2014-10-30 11:38:19 +0100

    Update helloworld.cpp
    Ajout d'une fonction perimRectangle(int longueur, int largeur)

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,6 +5,7 @@ using namespace std;
 //PROTOS
 int ajouteDeux(int x);
 int carre(int x);
+int perimRectangle(int longueur, int largeur);
 //CODE
 int main(int argc, char* argv[])
 {
@@ -20,4 +21,9 @@ int ajouteDeux(int x)
 int carre(int x)
 {
 	return x * x;
+}
+
+int perimRectangle(int longueur, int largeur)
+{
+	return (longueur + largeur) * 2;
 }
