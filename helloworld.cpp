afa3c2d3b6d9ca53ad598d26db4fda6f6e59b95b
Rubytox <c.fag@hotmail.fr>
2014-10-30 11:29:08 +0100

    Ajout de deux fichiers

---
+++ helloworld.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+//PROTOS
+int ajouteDeux(int x);
+int carre(int x);
+//CODE
+int main(int argc, char* argv[])
+{
+	cout << "Hello, world !" << endl;
+	return 0;
+}
+
+int ajouteDeux(int x)
+{
+	return x + 2;
+}
+
+int carre(int x)
+{
+	return x * x;
+}
